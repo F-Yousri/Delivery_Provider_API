1968934049e67c45f08dffc0f6449df44ca8e2d3
Minos32x <minasamy65@gmail.com>
2018-06-10 14:18:22 +0200

    limit driver data retrieved

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/api/v1/orders', to: 'orders#show'
-  post 'registerdriver' ,to: 'drivers#create'
+  post '/registerdriver' ,to: 'drivers#create'
   post '/api/v1/authentication/drivers/signin' ,to: 'authentication#authenticate'
   post '/api/v1/authentication/drivers/update' ,to: 'drivers#update'
   post '/api/v1/authentication/drivers/signout' ,to: 'drivers#signout'
