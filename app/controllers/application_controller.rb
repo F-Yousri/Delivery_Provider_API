32e6867ca3f6b7472a359c4f0dcc2de4e536ee3d
minaibrahimsaid <minaibrahim1991@yahoo.com>
2018-06-10 17:01:53 +0200

    sss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
     include Response
     include ExceptionHandler
-  
+
+    before_filter :cors_set_access_control_headers
+
+    def cors_set_access_control_headers
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = '*'
+        headers['Access-Control-Max-Age'] = "1728000"
+      end
     # called before every action on controllers
   
     attr_reader :current_driver
