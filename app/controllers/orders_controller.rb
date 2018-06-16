f81afe80a7e37667a023f6de12bc2e3389025be8
Minos32x <minasamy65@gmail.com>
2018-06-16 17:03:48 +0200

    merge and clean up un-necessary

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,16 +6,13 @@ class OrdersController < ApplicationController
         @vehicle = Vehicle.where("min_weight <= :weight AND max_weight >= :weight",
         {weight: order.weight.to_i, weight: order.weight.to_i})
         json_response response={ message: Message.no_vehicle } and return if @vehicle.empty? 
-        # json_response(@vehicle)
+    
         driver_response=DriversController.locations(order,@vehicle)
         vehicle=Vehicle.find_by_vehicle_kind(JSON.parse(driver_response)["driver"]["vehicle_kind"])
        
         # distance between order source and destination
         @src_dest_distance=Geocoder::Calculations.distance_between([order.src_latitude,order.src_longitude], [order.dest_latitude,order.dest_longitude])
-        # lat_lon = "#{order.src_latitude},#{order.src_longitude}"
-        # @city = Geocoder.search(lat_lon).first
-        # ss={city: @city.data['address_components'][4]}
-        # order.images= "localhost:3000"+order.images
+    
         order.cost= (@src_dest_distance*1300)*vehicle["vehicle_cost_rate"]
         order.status=1
         if JSON.parse(driver_response)['message']=='success'
