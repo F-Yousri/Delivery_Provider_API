3f9a2f6815ad0afeeec4d69cf4e62a1d550f9b94
F-Yousri <fahdyousri@gmail.com>
2018-06-16 11:12:39 +0200

    fix conflict

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,15 +3,10 @@ class OrdersController < ApplicationController
  require 'json'
     def show 
         order = Order.new(order_params)
-
         @vehicle = Vehicle.where("min_weight <= :weight AND max_weight >= :weight",
         {weight: order.weight.to_i, weight: order.weight.to_i})
-<<<<<<< HEAD
         json_response response={ message: Message.no_vehicle } and return if @vehicle.empty? 
         # json_response(@vehicle)
-=======
-      
->>>>>>> 82dbf64351a30e604bcb3441c4816c50af24414c
         driver_response=DriversController.locations(order,@vehicle)
         
         vehicle=Vehicle.find_by_vehicle_kind(JSON.parse(driver_response)["driver"]["vehicle_kind"])
@@ -24,7 +19,7 @@ class OrdersController < ApplicationController
 
         order.cost= (@src_dest_distance*1300)*vehicle["vehicle_cost_rate"]
         if JSON.parse(driver_response)['message']=='success'
-            order.save!
+            order.save
         end
         # parse response to add key& value to it
         driver_response=JSON.parse(driver_response)
@@ -34,6 +29,6 @@ class OrdersController < ApplicationController
     end
 
     def order_params
-        params.permit(:src_latitude,:src_longitude,:dest_latitude,:dest_longitude,:payment_method,:time,:title,:images,:weight,:description)
+        params.permit(:src_latitude,:src_longitude,:dest_latitude,:dest_longitude,:payment_method,:time,:title,:images,:weight)
     end
 end
